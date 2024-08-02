8b00fde4881cf4aca7f0147d6292f4722af0e24a
MZubrytski <maksim.zubrytski@hiqo-solutions.com>
2024-08-02 20:46:10 +0200

    fixed firebaseConfig.ts

--- firebaseConfig.ts
+++ firebaseConfig.ts
@@ -1,18 +1,35 @@
 // Import the functions you need from the SDKs you need
 import Constants from 'expo-constants';
 import { initializeApp } from 'firebase/app';
+// import { Platform } from 'react-native';
+// import 'dotenv/config';
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: Constants.manifest2?.extra?.firebaseApiKey,
-  authDomain: Constants.manifest2?.extra?.firebaseAuthDomain,
-  projectId: Constants.manifest2?.extra?.firebaseProjectId,
-  storageBucket: Constants.manifest2?.extra?.firebaseStorageBucket,
-  messagingSenderId: Constants.manifest2?.extra?.firebaseMessagingSenderId,
-  appId: Constants.manifest2?.extra?.firebaseAppId,
-};
+const firebaseConfig =
+  // Platform.OS === 'web'
+  //   ? {
+  //       apiKey: process.env.FIREBASE_API_KEY,
+  //       authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  //       projectId: process.env.FIREBASE_PROJECT_ID,
+  //       storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  //       messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  //       appId: process.env.FIREBASE_APP_ID,
+  //     }
+  //   :
+  {
+    apiKey: Constants.manifest2?.extra?.expoClient?.extra?.firebaseApiKey,
+    authDomain:
+      Constants.manifest2?.extra?.expoClient?.extra?.firebaseAuthDomain,
+    projectId: Constants.manifest2?.extra?.expoClient?.extra?.firebaseProjectId,
+    storageBucket:
+      Constants.manifest2?.extra?.expoClient?.extra?.firebaseStorageBucket,
+    messagingSenderId:
+      Constants.manifest2?.extra?.expoClient?.extra?.firebaseMessagingSenderId,
+    appId: Constants.manifest2?.extra?.expoClient?.extra?.firebaseAppId,
+  };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);

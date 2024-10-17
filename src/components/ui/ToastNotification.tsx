19ed387e5f0dde0fb2e270cbb82e8e9bebdb519a
MZubrytski <maksim.zubrytski@hiqo-solutions.com>
2024-10-17 11:30:35 +0200

    Created StyleSheet for the toast notification component

--- src/components/ui/ToastNotification.tsx
+++ src/components/ui/ToastNotification.tsx
@@ -1,5 +1,5 @@
 import { NOTIFICATION_WIDTH } from '@/constants/notification';
-import { Animated } from 'react-native';
+import { Animated, StyleSheet } from 'react-native';
 import { ToastProps } from 'react-native-toast-notifications/lib/typescript/toast';
 import { Text, View } from 'react-native-ui-lib';
 
@@ -8,28 +8,35 @@ export const ToastNotification = ({ toast }: { toast: ToastProps }) => {
     <View
       padding-8
       marginT-8
-      style={{
-        width: NOTIFICATION_WIDTH,
-        borderTopLeftRadius: 8,
-        borderTopRightRadius: 8,
-        borderBottomRightRadius: 8,
-        backgroundColor: toast.data.backgroundColor,
-      }}
+      style={[
+        styles.container,
+        { backgroundColor: toast.data.backgroundColor },
+      ]}
     >
       <Text center bodyMedium>
-        {toast.message} {toast.id}
+        {toast.message}
       </Text>
       <Animated.View
         style={[
-          {
-            position: 'absolute',
-            bottom: 0,
-            height: 2,
-            backgroundColor: toast.data.barColor,
-          },
-          { width: toast.data.barWidth },
+          styles.progressBar,
+          { backgroundColor: toast.data.barColor, width: toast.data.barWidth },
         ]}
       ></Animated.View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    position: 'relative',
+    width: NOTIFICATION_WIDTH,
+    borderTopLeftRadius: 8,
+    borderTopRightRadius: 8,
+    borderBottomRightRadius: 8,
+  },
+  progressBar: {
+    position: 'absolute',
+    bottom: 0,
+    height: 2,
+  },
+});
